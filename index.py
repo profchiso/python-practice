fa7d16e45dc9b2ff157088a3cc60d7162b359641
Chinedu Okorie <okoriechinedusunday@gmail.com>
2024-07-18 02:00:48 +0100

    python pratice

---
+++ index.py
@@ -0,0 +1,26 @@
+import sys
+print("hello")
+print(sys.version)
+#if 7 > 5:
+ #   print("yse")
+x=10
+y=30
+z=x+y
+print(z)
+
+print(type(x))
+
+print(str(x))
+print(float(x))
+
+firstName = "Chinedu"
+lastName = "Okorie"
+fullName = firstName + lastName
+print(fullName)
+
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
